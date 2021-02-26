692f90dda9be8e069b3d64fa105780064fb0222e
Ed-Games <edmarcosfilho2015@gmail.com>
2021-02-26 08:59:02 -0300

    create: Home page and styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import {ExperienceBar} from '../components/ExperienceBar'
+import Head from 'next/head'
+import React from 'react'
+import { Profile } from '../components/Profile'
+import styles from '../styles/pages/Home.module.css'
+import { CompletedChallenges } from '../components/completedChallenges'
+import { Countdown } from '../components/countdown'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.It</title>
+      </Head>
+      <ExperienceBar/>
+
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
